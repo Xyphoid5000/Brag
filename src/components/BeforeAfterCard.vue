09d7ffcc9367eeafd23dadb14e9a60c9d3ed3cd0
C90Gray <cmusic.gray@gmail.com>
2026-08-11 02:44:17 -0400

    added before and after photos to the gallery

---
+++ src/components/BeforeAfterCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  before: string
+  after: string
+  title: string
+}>()
+
+const showingAfter = ref(false)
+</script>
+
+<template>
+  <figure class="before-after-card" :class="{ 'before-after-card--after': showingAfter }">
+    <button class="before-after-card__image" type="button" :aria-label="`Show ${showingAfter ? 'before' : 'after'} image for ${title}`" @click="showingAfter = !showingAfter">
+      <img :src="showingAfter ? after : before" :alt="`${title} ${showingAfter ? 'after' : 'before'} view`" />
+      <span class="before-after-card__state">{{ showingAfter ? 'After' : 'Before' }}</span>
+      <span class="before-after-card__toggle">{{ showingAfter ? 'View before' : 'View after' }} <b>-></b></span>
+    </button>
+    <figcaption><span>BEFORE / AFTER</span><strong>{{ title }}</strong><small>Tap the image to compare the work.</small></figcaption>
+  </figure>
+</template>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -15,7 +15,7 @@
           <figcaption><span>01</span><strong>Back on the road</strong><small>Mobile service, wherever the day finds you.</small></figcaption>
         </figure>
         <figure class="gallery-card">
-          <img src="https://images.unsplash.com/photo-1487754180451-c456f719a1fc?auto=format&fit=crop&w=900&q=85" alt="Technician working on a vehicle" />
+          <img src="https://images.squarespace-cdn.com/content/v1/66b24961ed16e20fe92be52e/1786336795922-JVZ7SCTIYOF1XOBA3T9W/unsplash-image-NZ7fg_IVAzo.jpg?format=750w" alt="Technician working on a vehicle" />
           <figcaption><span>02</span><strong>Precise by hand</strong><small>Clean work. Careful finish.</small></figcaption>
         </figure>
         <figure class="gallery-card gallery-card--tall">
@@ -26,6 +26,21 @@
           <div class="gallery-card__statement"><p class="eyebrow"><span></span> The Burning River standard</p><h2>Clear glass.<br><em>Clear answers.</em></h2><RouterLink class="button button--orange" to="/contact">Start your quote <span>-></span></RouterLink></div>
           <figcaption><span>04</span><strong>Built for the drive ahead</strong><small>North East Ohio, at your location.</small></figcaption>
         </figure>
+        <BeforeAfterCard
+          title="A clearer finish"
+          before="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/648680057_122203230476351056_8717495280989062936_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1920&ctp=s1536x1920&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=ehSU5qsRH-QQ7kNvwGeayXV&_nc_oc=AdpdxH5MErCIbmWt7i13MbMrqNip03_sJAHAPD8O5DLcdNLBvnDImtHtWSwjiY0NEQcSFcD7oMlXdt1mzfNDx05k&_nc_zt=23&_nc_ht=scontent.fosu2-1.fna&_nc_gid=ymuKZS7dfh6fHJesB9LTuQ&_nc_ss=79289&oh=00_AQFRzRaidhNhYhL8C9BI4YC83JIMo1e-SWgcqAiTpH68ag&oe=6A8086F9"
+          after="https://scontent.fosu2-2.fna.fbcdn.net/v/t39.30808-6/648873664_122203230488351056_2727021876207523565_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1920&ctp=s1536x1920&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=eTsXLQC5mHkQ7kNvwHe3Sfm&_nc_oc=AdrQWUll237mL3m9Yg9c1BlJK5zoYxnCUfx0jjVTgxXHHyzYNpnxn260Z0B9Jcl8ZRnX3Lbd9x7T6zuMSpNYN6TO&_nc_zt=23&_nc_ht=scontent.fosu2-2.fna&_nc_gid=FUjc-yj-xUpYgny2dD7cvA&_nc_ss=79289&oh=00_AQF71rt5JBBj9cILUENZXI1xG3lxArVXrb37zztELXC9Mw&oe=6A807DC1"
+        />
+        <BeforeAfterCard
+          title="Detail that holds up"
+          before="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/492674464_122164632380351056_3227500911922058899_n.jpg?stp=dst-jpg_tt6&cstp=mx768x960&ctp=s768x960&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=PzByOm5yqjAQ7kNvwEhPCmr&_nc_oc=Adro4sNzvOpvLmBgYXUuX08lPg8o5ck_DS7yoy9hpc8XzCLXm5NdgWFzkPHKr53B1UIXwzhSyltJo73CiA5f-Y3Z&_nc_zt=23&_nc_ht=scontent.fosu2-1.fna&_nc_gid=mPNyD5AphPK0tHgZQktIZA&_nc_ss=79289&oh=00_AQGTlAnoErnNwZMQ1xJUCJNse_AsEDEhzyISw0ifKAQYcg&oe=6A808EFC"
+          after="https://scontent.fosu2-2.fna.fbcdn.net/v/t39.30808-6/493264554_122164632422351056_313326143885008509_n.jpg?stp=dst-jpg_tt6&cstp=mx768x960&ctp=s768x960&_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=F0Xrnc-Xoq4Q7kNvwGWmzrV&_nc_oc=AdohraBVOvjgBPi2cMI5lk4xEcsVSwQ_T5GgpHZmt0AVCoGnSA5CZOXriTmorrIHshx5XS2wx4sqiEOHO3CGKx0X&_nc_zt=23&_nc_ht=scontent.fosu2-2.fna&_nc_gid=QwIRMOcnnJHidf2DwbnayQ&_nc_ss=79289&oh=00_AQEfpwdpmhrZlnAPWeQCKNZI47mmLoP88eEMmiy86BHRPQ&oe=6A809D43"
+        />
+        <BeforeAfterCard
+          title="Ready for the road"
+          before="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/489353980_122161653860351056_3516435382668269870_n.jpg?stp=dst-jpg_tt6&cstp=mx768x960&ctp=s768x960&_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=4LcAbC2AFicQ7kNvwGG7qQp&_nc_oc=AdqhaDTlmOrJLce4CZPmU7ta7neVdQ18P6rjpzW0hXdkK77lGRiTDNK9oIp2xOexJntUEOwqyTr7zMPxvA7qON4E&_nc_zt=23&_nc_ht=scontent.fosu2-1.fna&_nc_gid=KajZn03XQuE5MkZI-GyTjg&_nc_ss=79289&oh=00_AQERmVBg4APwDIt1FYOUMhZy44enlVlWFisUqsSh-IPOHw&oe=6A80B013"
+          after="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/489957327_122161653908351056_257842339137853844_n.jpg?stp=dst-jpg_tt6&cstp=mx768x960&ctp=s768x960&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=fREg0-xw1-IQ7kNvwFySv3w&_nc_oc=Adp6J8vModv2HClI7nMipFfsFVdQMHBEgQ9Hj7CkIl68ahCIyQZH2kBFiAEWZcqIE-Xd6sNcq_J0dsgPHXxJw57L&_nc_zt=23&_nc_ht=scontent.fosu2-1.fna&_nc_gid=Vjk2K2OLp9h-nzNKRS-3zg&_nc_ss=79289&oh=00_AQE7SNa8MoPGQtHLSoSQgSxREIzEvK9VyxtT1vp3KD_5QQ&oe=6A8086F4"
+        />
       </section>
     </main>
     <footer class="landing-footer"><span>Burning River Auto Glass</span><span>Serving Cleveland and Northeast Ohio</span></footer>
@@ -34,5 +49,6 @@
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import BeforeAfterCard from '../components/BeforeAfterCard.vue'
 import SiteHeader from '../components/SiteHeader.vue'
 </script>
